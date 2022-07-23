ac4ea0550cd87df7cb9c01cf0281b33927f4b735
Alexander Drapoy <dr4alex@gmail.com>
2022-07-23 20:32:11 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,44 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const listOfCategories = document.querySelectorAll(`.item`);
+const statCategoriesNum = () =>
+  console.log(`Number of categories: ${listOfCategories.length}`);
+
+const setStatisticArr = (arr) => {
+  const list = [...arr].reduce((acc, item) => {
+    acc.push({
+      Catergory: item.querySelector(`h2`).textContent,
+      Elements: item.querySelectorAll(`li`).length,
+    });
+
+    return acc;
+  }, []);
+  return list;
+};
+
+const viewStat = (arr, setStatisticArr) => {
+  setStatisticArr(arr).map((item) => {
+    const keys = Object.keys(item);
+    for (const key of keys) {
+      console.log(`${key} : ${item[key]}`);
+    }
+  });
+};
+
+statCategoriesNum();
+viewStat(listOfCategories, setStatisticArr);
